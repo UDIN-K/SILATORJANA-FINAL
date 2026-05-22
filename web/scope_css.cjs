4fd89360a7bfae24557e5cf9a4ecd2ab811ec0ff
UDIN-K <safrisam.id09@gmail.com>
2026-05-22 20:48:37 +0700

    refactor: remove entire web directory and associated source files

--- web/scope_css.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('src/pages/pengusul/print.css', 'utf8');
-
-css = css.replace(/^(?!@media|\.print-page-wrapper| {8}\.print-page-wrapper)(\s*)([a-zA-Z\.#][^{]*?)\s*{/gm, '$1.print-page-wrapper $2 {');
-
-fs.writeFileSync('src/pages/pengusul/print.css', css);
-console.log('Scoped CSS successfully.');
--- web/scope_css.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('src/pages/pengusul/print.css', 'utf8');
-
-// We just want to prefix any rule that doesn't start with .print-page-wrapper, @media, or a few safe ones
-// Actually, it's safer to just inject `.print-page-wrapper ` before every selector block.
-css = css.replace(/^(?!@media|\.print-page-wrapper| {8}\.print-page-wrapper)(\s*)([a-zA-Z\.#][^{]*?)\s*{/gm, '$1.print-page-wrapper $2 {');
-
-fs.writeFileSync('src/pages/pengusul/print.css', css);
-console.log('Scoped CSS successfully.');
