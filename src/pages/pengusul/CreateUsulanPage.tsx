0fb756640d976bedf2838d0f18e6c4fc382a0c66
UDIN-K <safrisam.id09@gmail.com>
2026-05-23 20:35:43 +0700

    import api instance

--- src/pages/pengusul/CreateUsulanPage.tsx
+++ src/pages/pengusul/CreateUsulanPage.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { apiCreateKegiatan } from '@/lib/api';
+import api, { apiCreateKegiatan } from '@/lib/api';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
